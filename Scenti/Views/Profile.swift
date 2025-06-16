49dc0b6815b46e36de0cd00cc0409c7ebbe23041
atsentient <sentientao@gmail.com>
2025-06-16 21:54:00 +0400

    started working on profileview

--- Scenti/Views/Profile.swift
+++ Scenti/Views/Profile.swift
@@ -8,9 +8,30 @@
 import SwiftUI
 
 struct Profile: View {
+    @Environment(\.managedObjectContext) private var moc
+    
+    @FetchRequest(
+        entity: CDUserProfile.entity(),
+        sortDescriptors: [NSSortDescriptor(keyPath: \CDUserProfile.joinDate, ascending: false)]
+    ) var profiles: FetchedResults<CDUserProfile>
+    
+    private var currentProfile: CDUserProfile? {
+        profiles.first ?? createDefaultProfile()
+    }
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
+    
+    private func createDefaultProfile() -> CDUserProfile {
+        let newProfile = CDUserProfile(context: moc)
+        newProfile.id = UUID()
+        newProfile.name = "New user"
+        newProfile.joinDate = Date()
+        try? moc.save()
+        return newProfile
+    }
+    
 }
 
 #Preview {
